546870c6e07eb2d1e6507da0c8c0482346474e57
23Yong <1239824@naver.com>
2022-12-31 18:11:02 +0900

    feat: 프로젝트의 초기 환경설정 세팅
    별도로 branch를 분리하지 않고 바로 main 브랜치에 push한다.

---
+++ src/test/java/com/ytf/storageserver/StorageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytf.storageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
