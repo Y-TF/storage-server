6e91ffa59114c8a57890a95d00f983f67d9d0f65
23Yong <1239824@naver.com>
2023-02-01 14:39:34 +0900

    test: 이미지 다운로드 로직 테스트

--- src/test/java/com/ytf/storageserver/service/ImageServiceTest.java
+++ src/test/java/com/ytf/storageserver/service/ImageServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.util.StringUtils;
 
 import com.ytf.storageserver.dto.response.ImageUploadResponse;
+import com.ytf.storageserver.exception.ImageFileNotFoundException;
 
 @SpringBootTest
 class ImageServiceTest {
@@ -38,5 +40,33 @@ class ImageServiceTest {
 			() -> assertThat(StringUtils.getFilenameExtension(response.imageDownloadUrl()))
 				.isEqualTo("jpeg")
 		);
+	}
+
+	@DisplayName("이미지 다운로드 테스트")
+	@Nested
+	class ImageDownloadTest {
+		@DisplayName("이미지 다운로드 url이 주어지면 이미지를 다운로드하고 이미지 바이트를 반환한다.")
+		@Test
+		void givenImageUrl_whenDownloadImage_thenReturnsImageByteArray() {
+			// given
+			String imageUrl = "test-image.jpg";
+
+			// when
+			byte[] response = imageService.downloadImage(imageUrl);
+
+			// then
+			assertThat(response).isNotNull();
+		}
+
+		@DisplayName("경로에 없는 이미지 다운로드 url이 주어지면 ImageFileNotFoundException을 던진다.")
+		@Test
+		void givenWrongImageUrl_whenDownloadImage_thenThrowsImageFileNotFoundException() {
+			// given
+			String imageUrl = "wrong-image.jpg";
+
+			// when & then
+			assertThatThrownBy(() -> imageService.downloadImage(imageUrl))
+				.isInstanceOf(ImageFileNotFoundException.class);
+		}
 	}
 }
